57255cde41062a23f67481992721e3469bc8b082
Kenna <kenna.hwa@gmail.com>
2023-10-24 16:04:38 +0900

    Feat: 타이머 작동 함수 생성

--- day02/timer.js
+++ day02/timer.js
@@ -25,6 +25,7 @@ start_btn.addEventListener("click", (e)=>{
 	const sec = parseInt( seconds.value);
 
 	const totalSec = (hrs * 60 * 60) + (min * 60) + sec;
+	timerStart(totalSec);
 
 	/* 시분초 input 사라짐*/
 	[...input].forEach((input)=>{
@@ -32,3 +33,32 @@ start_btn.addEventListener("click", (e)=>{
 	})
 
 })
+
+/* 타이머 함수  */
+
+const timerStart = (totalTime) => {
+	i = setInterval(()=>{
+		totalTime--;
+
+		const real_hours =  Math.floor(totalTime / 60 / 60);
+		const real_minutes = Math.floor((totalTime / 60) % 60)
+		const real_seconds = totalTime % 60;
+
+		hour.value = real_hours;
+		minute.value = real_minutes;
+		seconds.value = real_seconds;
+
+	 /* 한 자리 숫자에 앞 0 붙이기 */
+
+		if(real_hours < 10){
+			hour.value = '0'+real_hours;
+		}
+		if(real_minutes < 10){
+			minute.value = '0'+real_minutes;
+		}
+		if(real_seconds < 10){
+			seconds.value = '0'+real_seconds;
+		}
+
+	},1000)
+}
