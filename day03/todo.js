2730e94a76cb954dd9c1225d1cac259d6be8d85c
Kenna <kenna.hwa@gmail.com>
2023-10-25 17:55:52 +0900

    Feat: day03 todo  기능 작성
    - 로컬스토리지 데이터 넣기

--- day03/todo.js
+++ day03/todo.js
@@ -2,17 +2,30 @@
 
 const form = document.getElementById('todoForm');
 
+
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-
+	const obj = {};
 	const formData = new FormData(form);
-	console.log('formData: ', [...formData]);
 
-	const obj = {'complete': "none"};
+	const todoItems =  localStorage.getItem("resatTodo") ? JSON.parse(localStorage.getItem("resatTodo")) : [];
 
 	formData.forEach((value, key) => obj[key] = value);
 
-	console.log("obj", obj)
-
-
+	localStorage.setItem("resatTodo",JSON.stringify([...todoItems, obj]))
 });
+
+
+
+/* 로딩 시 할 일 작성 내용 보이기 */
+
+const todo_list = document.querySelector(".todo_list");
+
+const data_arr = [];
+
+window.addEventListener("load", ()=>{
+
+
+
+
+})
