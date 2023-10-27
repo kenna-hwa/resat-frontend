2fbf5e9c300226711ed3416fea1e29f40dd9418a
Kenna <kenna.hwa@gmail.com>
2023-10-27 13:45:49 +0900

    Feat: day05 미니인턴 사이트 만들기
    - 폴더 생성

--- day04/calendar.js
+++ day04/calendar.js
@@ -1,85 +1,35 @@
-/* 기본 값 */
 let today = new Date();
-let nowDate = new Date();
-
-
-/* 캘린더 생성 */
-
-const createCalendar = (today) => {
-
-	let doMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-	let lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-	let calendarTable = document.querySelector(".cal_table > tbody");
-	
-	let row = calendarTable.insertRow();
-	let value = 1;
-
-	/* 일 월 생성 */
-	document.querySelector("#year").innerText = today.getFullYear();
-	document.querySelector("#month").innerText = today.getMonth() + 1;
-
-	 /* 기존 캘린더 삭제 */
-		while(calendarTable.rows.length > 0) {
-			calendarTable.deleteRow(calendarTable.rows.length - 1);
-	}
-
-
-let daysLength = (Math.ceil((doMonth.getDay() + lastDate.getDate()) / 7) * 7) - doMonth.getDay();
-
-/* 달력 출력 */
-
-for (let day = 1 - doMonth.getDay(); daysLength >= day; day++) {
-
-	let column = row.insertCell();
-
-	// @param 평일( 전월일과 익월일의 데이터 제외 )
-	if (Math.sign(day) == 1 && lastDate.getDate() >= day) {
-
-		// @param 평일 날짜 데이터 삽입
-		column.innerText = day;
-
-		// @param 일요일인 경우
-		if (value % 7 == 1) {
-			column.style.color = "#FF4500";
-		}
-
-		// @param 토요일인 경우
-		if (value % 7 == 0) {
-			column.style.color = "#0000CD";
-			row = calendarTable.insertRow();   // @param 토요일이 지나면 다시 가로 행을 한줄 추가한다.
-		}
-
-	}
-
-	// @param 평일 전월일과 익월일의 데이터 날짜변경
-	else {
-		let exceptDay = new Date(doMonth.getFullYear(), doMonth.getMonth(), day);
-		column.innerText = exceptDay.getDate(), 2;
-		column.style.color = "#D3D3D3";
-	}
-
-	value++;
+let final = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+if (today.getFullYear() % 4 == 0 && (today.getFullYear() % 100 != 0 || today.getFullYear() % 400 == 0)) {
+  final = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 }
-
-
+const  prevCalendar = () => {
+  today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+  makeArray();
 }
-window.addEventListener("load", () => {
-	createCalendar(today);
-})
-
-   /**
-     * @brief   이전달 버튼 클릭시
-     */
-	 const prevCalendar = () =>  {
-		today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
-		createCalendar(today); 
-}
-
-/**
- * @brief   다음달 버튼 클릭시
- */
 const nextCalendar = () => {
-	today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
-		createCalendar(today);
+  today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+  makeArray();
 }
+const makeArray = () => {
+  let first = new Date(today.getFullYear(), today.getMonth(), 1);
+  let day = 1;
+  let cal = new Array(6);
+  cal.fill(" ");
+  let j = first.getDay();
+  for (let i = 0; i < 6; i++) {
+    cal[i] = new Array(7);
+    cal[i].fill(" ");
+    for (j; j < 7; j++) {
+      cal[i][j] = day++;
+      if (day > final[today.getMonth()] + 1) cal[i][j] = " ";
+    }
+    j = 0;
+  }
+	console.log("cal,", cal)
+  // arrayToTable(cal);
+}
+
+window.addEventListener("load", () => {
+	makeArray();
+})
